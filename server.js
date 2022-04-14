889266fb4b32b84812dda6274fd1e41fe154cb54
Hugo Lemos <hugohl@live.unc.edu>
2022-04-14 16:21:12 -0400

    a04 v0.2

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
 const app = express();
-const args = require('minimist')(process.argv.slice(2));
-args['port', 'debug', 'log', 'help']
-const port = args.port || 5000
+const argv = require('minimist')(process.argv.slice(2));
+argv['port', 'debug', 'log', 'help']
+const port = argv.port || 5000
 
 debug = false;
 
@@ -33,7 +33,7 @@ app.get('/app/', (req, res) => {
         res.end(res.statusCode + ' ' + res.statusMessage);
 });
 
-if (args.help || args.h) {
+if (argv.help || argv.h) {
   console.log("server.js [options]")
   console.log("--port	Set the port number for the server to listen on. Must be an integer between 1 and 65535.");
   console.log("--debug If set to `true`, creates endlpoints /app/log/access/ which returns a JSON access log from the database and /app/error which throws an error with the message \"Error test successful.\" Defaults to `false`.");
