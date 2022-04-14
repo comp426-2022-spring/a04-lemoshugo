766fc91c71bb036fcd03fcda1b1705dc6744d61b
Hugo Lemos <hugohl@live.unc.edu>
2022-04-14 16:07:15 -0400

    a04 v0

---
+++ database.js
@@ -0,0 +1,30 @@
+const Database = require('better-sqlite3');
+
+const db = new Database('log.db');
+
+const stmt = db.prepare(`
+SELECT name FROM sqlite_master WHERE type='table' and name='userinfo';`);
+
+let row = stmt.get();
+
+if (row == undefined) {
+    const sqlInit = `
+        CREATE TABLE accesslog (
+            accesslogid INTEGER PRIMARY KEY, 
+            remoteaddr TEXT, 
+            remoteuser TEXT, 
+            time TEXT, 
+            method TEXT, 
+            url TEXT, 
+            protocol TEXT,
+            httpversion TEXT, 
+            status TEXT, 
+            referer TEXT,
+            useragent TEXT
+        );`
+    db.exec(sqlInit);
+} else {
+    console.log('Database exists.');
+}
+
+module.exports = db;
